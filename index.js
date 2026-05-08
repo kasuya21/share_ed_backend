ef1eea371c9169a38dd2e2488f1bba2d31107963
028Siwakorn <664259028@webmail.npru.ac.th>
2026-05-08 15:24:01 +0700

    Add feat/shop_item

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import "dotenv/config";
 
-// import userRouter from "./routes/user.routes.js";
+
 import authRoutes from "./routers/auth.router.js";
+import shopItemRoutes from "./routers/shopItem.router.js";
 
 
 
@@ -12,9 +13,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes
-// app.use("/api/v1/user", userRouter);
+
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/shop-items", shopItemRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
