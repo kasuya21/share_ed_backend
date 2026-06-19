f586317c31d112bf1b446216782a7ea9ff17964d
kasuya21 <thunva2004@gmail.com>
2026-06-19 14:20:22 +0700

    fix: revert pdf resource_type back to raw for correct cloudinary upload

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -27,7 +27,7 @@ async function handleMediaFiles(files, postId) {
     const uploadOptions = isPdf
       ? { 
           folder: `share-ed/posts/${postId}/pdfs`, 
-          resource_type: "auto",
+          resource_type: "raw",
           public_id: file.originalname.replace(/\.[^/.]+$/, "").replace(/\s+/g, '_')
         }
       : {
