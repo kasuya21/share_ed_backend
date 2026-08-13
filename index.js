0948bc4a633aaab61257aaae44a9010f6dfa6f57
Siwakorn028 <664259028@webmail.npru.ac.th>
2026-08-14 05:11:58 +0700

    Add corsOptions aws-share-ed

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const corsOptions = {
     "https://share-ed-backend-6jer.onrender.com",
     "https://share-ed-frontend-gamma.vercel.app",
     "https://share-ed-frontend-iota.vercel.app",
-    "https://github.com/kasuya21/share_ed_backend.git",
+    "http://aws-sharehub-app-2026-frontend.s3-website-us-east-1.amazonaws.com/",
   ],
   credentials: true,
 };
